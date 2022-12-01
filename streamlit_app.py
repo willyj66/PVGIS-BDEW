4d6714785731cd141b34e0363caceab1452c9299
Maxim <maxim.oweyssi@est.org.uk>
2022-12-01 16:21:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,8 @@ with col2:
     workday = pd.DataFrame({'t': t,'y':df[month-1]['BDEW workday']})
     saturday = pd.DataFrame({'t': t,'y':df[month-1]['BDEW saturday']})
     sunday = pd.DataFrame({'t': t,'y':df[month-1]['BDEW sunday']})
-    st.altair_chart(alt.Chart(PV,height=600)
-    .mark_line(color='#0068c9', opacity=0.5)
-    .encode(x='t', y='y'),
+    chart = alt.Chart(PV,height=600).mark_line(color='#0068c9', opacity=0.5).encode(x='t', y='y')
+    st.altair_chart(chart,
     use_container_width=True)
         
 
