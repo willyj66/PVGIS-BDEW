7451092f79b61e822c105833e94ddfb722eccef3
Maxim <maxim.oweyssi@est.org.uk>
2022-12-01 14:57:39 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,10 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import pgeocode
 
 
-
+country = pgeocode.Nominatim("gb")
 MonthDict={ 1 : "January", 2 : "February", 3 : "March", 4 : "April", 5 : "May", 6 : "June", 7: "July",
             8 : "August", 9 : "September", 10 : "October", 11 : "November",12 : "December"}
 PropertyDict={
@@ -28,12 +29,22 @@ Made by Maxim Oweyssi for the Energy Saving Trust :heart:
 
 Imput your property parameters, proposed PV install specifications, annual consumption and press Submit!
 """
+
 @st.cache
 def to_the_shop_to_get_your_PVGIS_data(property_type,annual_consumption, PV_max_power, surface_tilt, surface_azimuth):
     return makedf(invPropertyDict[property_type],lat, lon, annual_consumption, PV_max_power, surface_tilt, surface_azimuth,start, end)
 
 with st.form(key="Input parameters"):
     property_type = st.selectbox('What is the property type?',PropertyDict.values())
+    location_method = st.selectbox('How would you like to input location?',('Postcode','Coordinates'))
+    if location_method == 'Postcode':
+        postcode = st.text_input('Postcode')
+        postcode_data = country.query_postal_code(postcode)
+        lat = postcode_data["latitude"].values[0]
+        lon = postcode_data["longitude"].values[0]
+    elif location_method == 'Coordinates':
+        lat = float(st.text_input('Latitude'))
+        lat = float(st.text_input('Longitude'))
     annual_consumption = st.number_input('Annual property consumption [kWh]',value=12000,step=1)
     PV_max_power = st.number_input('PV system peak power [kWp]',value=5,step=1)
     surface_tilt = st.number_input('Surface tilt [degrees]',value=35,step=1)
@@ -43,6 +54,9 @@ with st.form(key="Input parameters"):
         df, average,cloudy, sunny, bdew_demand, t, yearly_gen, yearly_use = to_the_shop_to_get_your_PVGIS_data(
             property_type,annual_consumption, PV_max_power, surface_tilt, surface_azimuth)
         month = st.slider("Month", 1, 12, 12)
+
+
+
         PV = df[month-1]['PV generation']
         source = pd.DataFrame({'t': t,'y': PV})
         st.altair_chart(alt.Chart(pd.DataFrame(source), height=500, width=500)
