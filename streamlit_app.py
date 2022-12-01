ef23fc50c96c4fe8fce1fd40e898c41b0e01127d
Maxim <maxim.oweyssi@est.org.uk>
2022-12-01 15:35:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ Made by Maxim Oweyssi for the Energy Saving Trust :heart:
 Imput your property parameters, proposed PV install specifications, annual consumption and press Submit!
 """
 
-col1, col2 = st.columns(2)
+
 @st.cache
 def to_the_shop_to_get_your_PVGIS_data(property_type,lat,lon,annual_consumption, PV_max_power, surface_tilt, surface_azimuth):
     return makedf(invPropertyDict[property_type],lat, lon, annual_consumption, PV_max_power, surface_tilt, surface_azimuth,start, end)
@@ -43,30 +43,31 @@ with st.form(key="Location parameters"):
     
     
 with st.form(key="Input parameters"):
-    with col1:
-        if location_method == 'Coordinates':
-            lat = float(st.text_input('Latitude', value=56.140,))
-            lon = float(st.text_input('Longitude',value =-3.919))
-        if location_method == 'Postcode':
-            postcode = st.text_input('Postcode')
-            postcode_data = country.query_postal_code(postcode)
-            lat = postcode_data["latitude"]
-            lon = postcode_data["longitude"]
-        annual_consumption = st.number_input('Annual property consumption [kWh]',value=12000,step=1)
-        PV_max_power = st.number_input('PV system peak power [kWp]',value=5,step=1)
-        surface_tilt = st.number_input('Surface tilt [degrees]',value=35,step=1)
-        surface_azimuth = st.number_input('Surface tilt [degrees]',value=0,step=1)
+    if location_method == 'Coordinates':
+        lat = float(st.text_input('Latitude', value=56.140,))
+        lon = float(st.text_input('Longitude',value =-3.919))
+    if location_method == 'Postcode':
+        postcode = st.text_input('Postcode')
+        postcode_data = country.query_postal_code(postcode)
+        lat = postcode_data["latitude"]
+        lon = postcode_data["longitude"]
+    annual_consumption = st.number_input('Annual property consumption [kWh]',value=12000,step=1)
+    PV_max_power = st.number_input('PV system peak power [kWp]',value=5,step=1)
+    surface_tilt = st.number_input('Surface tilt [degrees]',value=35,step=1)
+    surface_azimuth = st.number_input('Surface tilt [degrees]',value=0,step=1)
     button = st.form_submit_button(label="Plot the plot!")
     if button:
         df, average,cloudy, sunny, bdew_demand, t, yearly_gen, yearly_use = to_the_shop_to_get_your_PVGIS_data(
             property_type,lat,lon,annual_consumption, PV_max_power, surface_tilt, surface_azimuth)
-        with col2:
-            month = st.slider("Month", 1, 12, 12)
-            PV = df[month-1]['PV generation']
-            source = pd.DataFrame({'t': t,'y': PV})
-            st.altair_chart(alt.Chart(pd.DataFrame(source), height=500, width=500)
-            .mark_line(color='#0068c9', opacity=0.5)
-            .encode(x='t', y='y'))
+        month = st.slider("Month", 1, 12, 12)
+
+
+
+        PV = df[month-1]['PV generation']
+        source = pd.DataFrame({'t': t,'y': PV})
+        st.altair_chart(alt.Chart(pd.DataFrame(source), height=500, width=500)
+        .mark_line(color='#0068c9', opacity=0.5)
+        .encode(x='t', y='y'))
         
 
 
