575458ec300666fda76a5d781c7ba297b63aa365
Maxim <maxim.oweyssi@est.org.uk>
2022-12-06 11:52:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ with col3:
                                     data=toexport ,
                                     file_name= invPropertyDict[property_type]+"_"+str(annual_consumption)+"kWh_"+str(PV_max_power)+"kWp.xlsx")
         logo = Image.open('logo.png')
-        st.text(" \n \n \n \n \n")
+        st.write('###')
         st.image(logo)
 
 
